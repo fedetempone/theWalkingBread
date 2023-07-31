e6ea410ab526a29b54e779f34d94f31859fc9c19
fedetempone <fedetempone5@gmail.com>
2023-07-31 17:01:41 -0300

    replace browser router because is unsopported

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Home from './components/home/Home';
 import Contact from './components/contact/Contact'
 import Cart from './components/cart/Cart';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route, HashRouter } from 'react-router-dom';
 import Products from './components/products/Products';
 import AboutUs from './components/aboutUs/AboutUs';
 
 function App() {
   return (
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home/>} />  
           <Route path="/contact" element={<Contact/>} />  
@@ -15,7 +15,7 @@ function App() {
           <Route path="/products" element={<Products/>} />  
           <Route path="/about" element={<AboutUs/>} />  
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
   );
 }
 
